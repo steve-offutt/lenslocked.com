28f2cb03da7e821bdd029ea974dd7a91c8b5ef4f
Steve Offutt <stvoffutt@gmail.com>
2021-03-23 21:37:19 -0500

    add home and contact handlers

--- main.go
+++ main.go
@@ -20,10 +20,20 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `To get in touch, please send and email to <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
