e0eb4352a56e16039b2d35c5f5fd8336e7cb41b0
Steve Offutt <stvoffutt@gmail.com>
2021-03-23 21:37:43 -0500

    remove old handlerfunc

--- main.go
+++ main.go
@@ -7,19 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, `To get in touch, please send and email to <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you are looking for</h1>")
-	}
-}
-
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, `To get in touch, please send and email to <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
