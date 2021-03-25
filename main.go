e64331e7c0b8ecb7bd5bf966398fc6aaa80097e4
Steve Offutt <stvoffutt@gmail.com>
2021-03-24 19:17:12 -0500

    exercises

--- main.go
+++ main.go
@@ -17,10 +17,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	faq := `<h1>Awesome site FAQ</h1><ul><li>question 1</li><li>question 2</li></ul>`
+	fmt.Fprint(w, faq)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	faq := `<h1>NOT FOUND DAWG</h1>`
+	fmt.Fprint(w, faq)
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
